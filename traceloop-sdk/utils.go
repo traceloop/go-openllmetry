790fb00409c337cc21f0753940007a89e362dc22
Gal Kleinman <gal.klm@gmail.com>
2024-01-17 09:51:49 +0200

    naming change

--- traceloop-sdk/utils.go
+++ traceloop-sdk/utils.go
@@ -8,7 +8,7 @@ import (
 	"github.com/cenkalti/backoff"
 )
 
-func (sdk *Traceloop) GetVersion() string {
+func (instance *Traceloop) GetVersion() string {
 	info, ok := debug.ReadBuildInfo()
     if !ok {
         fmt.Printf("Failed to read build info")
@@ -18,24 +18,24 @@ func (sdk *Traceloop) GetVersion() string {
 	return info.Main.Version
 }
 
-func (sdk *Traceloop) fetchPath(path string) (*http.Response, error) {
-	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/%s", sdk.Config.BaseURL, path), nil)
+func (instance *Traceloop) fetchPath(path string) (*http.Response, error) {
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/%s", instance.Config.BaseURL, path), nil)
 	if err != nil {
 		return nil, err
 	}
 
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", sdk.Config.APIKey))
-	req.Header.Set("X-Traceloop-SDK-Version", sdk.GetVersion())
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", instance.Config.APIKey))
+	req.Header.Set("X-Traceloop-SDK-Version", instance.GetVersion())
 
-	return sdk.Client.Do(req)
+	return instance.Client.Do(req)
 }
 
-func (sdk *Traceloop) fetchPathWithRetry(path string, maxRetries uint64) (*http.Response, error) {
+func (instance *Traceloop) fetchPathWithRetry(path string, maxRetries uint64) (*http.Response, error) {
 	var resp *http.Response
 	
 	err := backoff.Retry(func() error {
 		var err error
-		resp, err = sdk.fetchPath(path)
+		resp, err = instance.fetchPath(path)
 		return err
 	}, backoff.WithMaxRetries(backoff.NewExponentialBackOff(), maxRetries))
 
